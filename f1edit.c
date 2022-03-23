554e44cb44d8b45fc3eca4cff33f93b04c401cae
yetmorecode <yetmorecode@users.noreply.github.com>
2022-03-23 15:44:58 +0100

    Added directories to 3D

--- f1edit.c
+++ f1edit.c
@@ -1,40 +1,51 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <direct.h>
 
 typedef struct {
 	uint32_t offset;
 	uint32_t size;
-	uint16_t extra;
+	uint16_t dir;
 	char name[14];
-} entry;
+} file_entry;
+
+typedef struct {
+	char name[80];
+} dir_entry;
 
 int main (int ac, char**av) {
 	FILE *file = fopen(av[1], "rb");
 
-	unsigned int a, b;
-	fread(&a, 4, 1, file);
-	fread(&b, 4, 1, file);
+	int files, directories;
+	fread(&files, 4, 1, file);
+	fread(&directories, 4, 1, file);
 
-	entry e;
-	FILE *index = fopen("3d\\_index.txt", "w");
-	for (int i=0; i < a; i++) {
-		fread(&e, sizeof(e), 1, file);
-		fprintf(index, "%13s: offset = %06x, length = %06d, extra = %04x\n", e.name, e.offset, e.size, e.extra);
-
+	file_entry *entries = (file_entry*)malloc(sizeof(file_entry)*files);
+	fread(entries, sizeof(file_entry), files, file);
+	dir_entry *dirs = (dir_entry*)malloc(sizeof(dir_entry)*directories);
+	fread(dirs, sizeof(dir_entry), directories, file);
+	FILE *index = fopen("3D\\index.txt", "w");
+	for (int i=0; i < directories; i++) {
+		mkdir(dirs[i].name);
+	}
+	for (int i=0; i < files; i++) {
+	    	file_entry *e = entries+i;
+		fprintf(index, "%s\\%13s: offset = %06x, length = %06d\n",
+			dirs[e->dir].name,
+			e->name, e->offset, e->size);
 		char name[256];
-		sprintf(name, "3d\\%s", e.name);
+		sprintf(name, "%s\\%s", dirs[e->dir].name, e->name);
+		//printf("%s\n", name);
 		FILE  *out = fopen(name, "wb");
-		char *buf = (char*)malloc(e.size);
-		long pos = ftell(file);
-		fseek(file, e.offset, SEEK_SET);
-		fread(buf, e.size, 1, file);
-		fseek(file, pos, SEEK_SET);
-		fwrite(buf, e.size, 1, out);
+		char *buf = (char*)malloc(e->size);
+		fseek(file, e->offset, SEEK_SET);
+		fread(buf, e->size, 1, file);
+		fwrite(buf, e->size, 1, out);
 		free(buf);
 		fclose(out);
 	}
-	printf("%d total\n", a);
+	printf("%d files, %d directories\n", files, directories);
 	fclose(file);
 
 	return 0;
